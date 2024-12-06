9fb299ad862a8f5d1669f4ebd42523998d1750e7
Sam Ollari <saollari@wpi.edu>
2024-12-05 21:26:41 -0500

    Bring in headers and makefile. It builds

---
+++ sys/netinet/cc/cc_search_common.h
@@ -0,0 +1,33 @@
+#ifndef _CC_SEARCH_COMMON_H
+#define _CC_SEARCH_COMMON_H
+
+#define MAX_SEARCH_BIN_VALUE 0xFFFF
+
+typedef uint16_t search_bin_t;
+
+/**
+ * Window size, in tenths of an RTT (35 = 3.5*InitRTT)
+ */
+#define SEARCH_WINDOW_SIZE_TIME 35
+#define SEARCH_BINS 10
+#define SEARCH_EXTRA_BINS 15
+#define SEARCH_TOTAL_BINS SEARCH_BINS + SEARCH_EXTRA_BINS
+/**
+ * Slow start exit threshold, in percent
+ */
+#define SEARCH_THRESH 35
+/** How many bits to shift each iteration trying to fit sequence number in a bin */
+#define SEARCH_SCALE_SHIFT_STEP 1
+/** Force a reset after missing this number of bins */
+#define SEARCH_MISSED_BIN_COUNT_TRIGGER_RESET 3
+/**
+ * Enable cwnd rollback (on exit, reduce cwnd to its value 2 InitRTT ago, to avoid congestion)
+ */
+#define SEARCH_ROLLBACK 1
+
+#define SEARCH_WINDOW_SIZE(initial_rtt) initial_rtt * SEARCH_WINDOW_SIZE_TIME
+#define SEARCH_BIN_DURATION(initial_rtt) SEARCH_WINDOW_SIZE(initial_rtt) / SEARCH_BINS
+
+#define SEARCH_BIN(ccv, index) ((struct newreno*)(ccv)->cc_data)->search_bin[(index) % SEARCH_TOTAL_BINS]
+
+#endif
